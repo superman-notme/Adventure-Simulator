aaa8f7bed1e2964022e1b6752329c3d527584245
Brandon Lee Tharp <91434053+superman-notme@users.noreply.github.com>
2021-09-26 16:14:15 -0400

    Create weapon.py

---
+++ weapon.py
@@ -0,0 +1,9 @@
+class Weapon:
+  def __init__(self, name, price, con, atk):
+    self.name = name
+    self.price = int(price)
+    self.con = int(con)
+    self.atk = int(atk)
+    
+  def __repr__(self):
+    return self.name + ': ' + str(self.con) + ' concealment, ' + str(self.atk) + ' attack'
