84d36352a0f29adc786e6051ced24e7fa82ef218
Brandon Lee Tharp <91434053+superman-notme@users.noreply.github.com>
2021-09-26 16:16:02 -0400

    Create robbery.py

---
+++ robbery.py
@@ -0,0 +1,8 @@
+class Robbery:
+  def __init__(self, name, sec, res, lootMax, lootDesc, infamy):
+    self.name = name
+    self.sec = int(sec)
+    self.res = int(res)
+    self.lootMax = int(lootMax)
+    self.lootDesc = lootDesc
+    self.infamy = infamy
